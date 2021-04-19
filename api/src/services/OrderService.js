f4314e2f61b6471d784885e980b344bac295d5d0
Felipe Lemos <felipe.lemos@dataprev.gov.br>
2021-04-19 20:58:13 -0300

    Added docker-composer for kafka and producer

--- api/src/services/OrderService.js
+++ api/src/services/OrderService.js
@@ -1,9 +1,26 @@
 const { Kafka } = require('kafkajs')
 
+const kafka = new Kafka({
+    clientId: "order-producer",
+    brokers: ['kafka:9092']
+})
+
+const producer = kafka.producer()
+
 module.exports = {
     async createOrder(request, response) {
 
         //inserir aqui o kafka producer.
+        await producer.connect()
+        await producer.send({
+            topic: 'test-topic',
+            messages: [
+                { value: 'Hello KafkaJS user!' },
+            ],
+        })
+
+        await producer.disconnect()
         return response.json({ ok: true })
     }
+
 }
